e102bb56a5155c1e64c5eb91c8dc883e3dc14891
Y-T-G <32206511+Y-T-G@users.noreply.github.com>
2025-09-09 14:12:46 +0800

    Decouple forward from loss

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -414,8 +414,8 @@ class BaseTrainer:
                 # Forward
                 with autocast(self.amp):
                     batch = self.preprocess_batch(batch)
-                    metadata = {k: batch.pop(k, None) for k in ["im_file", "ori_shape", "resized_shape"]}
-                    loss, self.loss_items = self.model(batch)
+                    preds = self.model(batch["img"])
+                    loss, self.loss_items = self.model.loss(batch, preds)
                     self.loss = loss.sum()
                     if RANK != -1:
                         self.loss *= world_size
@@ -456,7 +456,6 @@ class BaseTrainer:
                     )
                     self.run_callbacks("on_batch_end")
                     if self.args.plots and ni in self.plot_idx:
-                        batch = {**batch, **metadata}
                         self.plot_training_samples(batch, ni)
 
                 self.run_callbacks("on_train_batch_end")
